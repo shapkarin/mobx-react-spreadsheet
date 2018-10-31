dfde3b91053d1a09888f6cda4c3745150ad2d09c
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-10-31 10:59:35 +0300

    determineDisplay object arg

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -14,7 +14,8 @@ export default class Row extends React.Component<RowProps> {
   icons: Icon[];
   fields: string[];
 
-  determineDisplay = (value, field) => {
+  determineDisplay = (options) => {
+    const { value, field } = options;
     const index = this.fields.indexOf(field);
 
     if (index !== -1) {
@@ -37,7 +38,7 @@ export default class Row extends React.Component<RowProps> {
     for (let key in this.props.rowData) {
       // not reusable
       if (key !== '_red') {
-        value = this.determineDisplay(this.props.rowData[key], key);
+        value = this.determineDisplay({value: this.props.rowData[key], field: key});
         result.push(
           <Cell
             editCell={this.editCell}
