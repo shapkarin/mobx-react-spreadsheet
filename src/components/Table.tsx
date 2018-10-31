20eeb16150c816c2be3c3a61eb668cbf755063ff
Yury Shapkarin <yu.shapkarin@gmail.com>
2018-10-31 09:43:55 +0300

    stateless observer component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,40 +1,33 @@
 import React from 'react';
 import Row from './Row';
 import { observer } from 'mobx-react';
-import { TableProps } from './types';
+// import { TableProps } from './types';
 
-//you can use stateless component https://mobx.js.org/refguide/observer-component.html
-@observer
-export default class Table extends React.Component<TableProps> {
+const Table = observer(({ store, options }) =>
+  <table className="Table">
+    <thead>
+      <tr>
+      {store.columns.map((item) => (
+          <th className="Table_Cell">{item.header}</th>
+      ))}
+      </tr>
+    </thead>
+    <tfoot>
+      <tr><td><button onClick={() => store.addRow()}>Add row</button></td></tr>
+    </tfoot>
+    <tbody>
+    {store.table.map((item, idx) => (
+      <Row
+        rowData={item}
+        key={idx}
+        id={idx}
+        className={item._red ? 'Row_red' : ''}
+        options={options}
+        removeRow={store.removeRow}
+      />
+    ))}
+    </tbody>
+  </table>
+);
 
-  render() {
-    const { store, options } = this.props;
-
-    return (
-      <table className="Table">
-        <thead>
-          <tr>
-          {store.columns.map((item) => (
-              <th className="Table_Cell">{item.header}</th>
-          ))}
-          </tr>
-        </thead>
-        <tfoot>
-          <tr><td><button onClick={() => store.addRow()}>Add row</button></td></tr>
-        </tfoot>
-        <tbody>
-        {store.table.map((item, idx) => (
-          <Row
-            rowData={item}
-            key={idx}
-            id={idx}
-            className={item._red ? 'Row_red' : ''}
-            options={options}
-            removeRow={store.removeRow}
-          />
-        ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+export default Table;
